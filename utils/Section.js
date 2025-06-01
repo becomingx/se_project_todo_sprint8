780a0397c1761ed744b4421a011ba18b0a62853d
becomingx <patriciatirado9@gmail.com>
2025-06-01 17:09:41 -0400

    commit sprint 8

--- utils/Section.js
+++ utils/Section.js
@@ -6,7 +6,8 @@ export default class Section {
     constructor({ items= [], renderer = () => {}, containerSelector }) {
         this._items = items; 
         this._renderer = renderer;
-        this._containerSelector = document.querySelector(containerSelector);    }
+        this._containerElement = document.querySelector(containerSelector);    
+    };
 
 
         renderItems() {
@@ -15,22 +16,22 @@ export default class Section {
 
         //This method should be called once on page load
         //renders all elements on page
-        //iterate through items array, call renderer() function on each item in array
-        // Call the renderer function for each item
+        //item = raw data from _items array
+   
             this._items.forEach(item => {
-                this._renderer(item); 
+                const renderedItem = this._renderer(item);
+                return this.addItem(renderedItem);
             });
-        }
+        };
     
         addItem(element) {
         //This method should be called when adding an individual card to the DOM
         //takes a DOM element and adds it to the container
             if (element instanceof HTMLElement) {
-                this._containerSelector.append(element); // Append the element to the container
+                const container = this._containerElement;
+                container.append(element);
             } else {
                 console.error('Invalid element provided to addItem method');
             }
-        }
-
-
+        };
 };
